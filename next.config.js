89c59dfc0d2ec1e3626da90d9af26e4995c8430d
jsstackdevelopers <javascriptstack0011@gmail.com>
2022-01-02 03:59:25 +0500

    bug fix improvement

--- next.config.js
+++ next.config.js
@@ -3,4 +3,8 @@ module.exports = {
   images: {
     domains: ["designcode.io", "www.google.com"],
   },
+  env: {
+    API_KEY: 'AIzaSyA90BUDoMmC0eMnPmOEb1j_WpHeIk-_zo4',
+    CONTEXT_KEY: '1a8ce88d8f3d84c11'
+  }
 }
